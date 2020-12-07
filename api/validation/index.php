d970a98461d1303a5f2a60c208ef3db905652fcf
Ryan <ryan1230tenerife@gmail.com>
2020-12-07 14:56:35 +0000

    class created with functions

---
+++ api/validation/index.php
@@ -0,0 +1,73 @@
+<?php
+class ErrorHandler {
+	
+	/*
+	 * check is any of the values are emtpy
+	 * if there is an empty value execute the 
+	 * callback function
+	 * */
+	public function validate_empty_values(array $json_values) {
+		foreach($json_values as $value) {
+			if(emtpy($value)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/*
+	 * sanitize the array of strings
+	 * return false if the filter fails
+	 * */
+	public function sanitize_string(array $strings) {
+		foreach($strings as $value) {
+			if(!filter_var($value, FILTER_SANITIZE_STRING)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/*
+	 * sanitize the array of numbers
+	 * return false if the filter fails
+	 * */
+	public function sanitize_int(array $numbers) {
+		foreach($numbers as $value) {
+			if(!filter_var($value, FILTER_SANITIZE_NUMBER_INT) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/*
+	 * validate the array of string against a regex pattern
+	 * return false if the values do not match
+	 * */
+	public function validate_string(array $strings) {
+		
+		$pattern = "^[a-z A-Z]+$";
+		$options = array("options" => array("regexp" => $pattern));
+		
+		foreach($strings as $value) {
+			if(!filter_var($value,  FILTER_VALIDATE_REGEX, $options)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/*
+	 * validate the array of integers
+	 * return false if the values are not integers
+	 * */
+	public function validate_int(array $numbers) {
+		foreach($numbers as $value) {
+			if(!filter_var($value, FILTER_VALIDATE_INT)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
