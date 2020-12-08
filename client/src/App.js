72aa98dae555372bab06a0dc1282f33ebd06c54e
Ryan <ryan1230tenerife@gmail.com>
2020-12-08 21:13:08 +0000

    setup entry point to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// Import stylesheets
+import "./css/App.css";
+
+// Import dependencies
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+// Import Components
+import Header from "./components/Header";
+
+// Import Pages
+import Home from "./pages/Home";
+import CreateTicket from "./pages/CreateTicket";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* Here are all the components that belong
+          on all pages
+       */}
+      <Header />
+      
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/create">
+          <CreateTicket />
+        </Route>
+      </Switch>
+
+    </Router>
   );
 }
+
 
 export default App;
