9fd884e8d682d3cbf170af0f0332b8a4c08ab307
Ryan <ryan1230tenerife@gmail.com>
2020-12-08 21:10:17 +0000

    created pages

---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import TicketTable from "../components/TicketTable";
+
+function Home() {
+    return(
+        <main className="container">
+            <TicketTable />
+        </main>
+    )
+}
+
+export default Home;
