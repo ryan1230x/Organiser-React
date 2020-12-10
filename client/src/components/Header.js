41169af7807807855e55120c9803def33c4da8d0
Ryan <ryan1230tenerife@gmail.com>
2020-12-10 08:00:08 +0000

    minor changes to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,44 +5,49 @@ class Header extends React.Component {
   render() {
     return (
       <header className="d-flex align-items-center">
-        <div className="d-flex align-items-center">
-          <Link to="/">
-            <img
-              width="125px"
-              src="https://direct-telecom.es/wp-content/uploads/2014/08/dt.png"
-              className="img-fluid"
-              alt="logo"
-            />
-          </Link>
-        </div>
-        <div className="navbar-nav d-flex">
-          <ul className="d-flex">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/history">
-                History
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/create">
-                Create Ticket
-              </Link>
-            </li>
-          </ul>
-          <form className="d-flex align-items-center ml-auto">
-            <div className="d-flex" style={{ position: "relative", left: 30 }}>
-              <ion-icon style={{ fontSize: "1.4rem" }} name="search"></ion-icon>
-            </div>
-            <input
-              className="form-inline"
-              type="text"
-              placeholder="Search..."
-            />
-          </form>
+        <div className="container d-flex">
+          <div className="d-flex align-items-center">
+            <Link to="/">
+              <img
+                width="100px"
+                src="https://direct-telecom.es/wp-content/uploads/2014/08/dt.png"
+                className="img-fluid"
+                alt="logo"
+              />
+            </Link>
+          </div>
+          <div className="navbar-nav d-flex">
+            <ul className="d-flex">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/history">
+                  History
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/create">
+                  Create Ticket
+                </Link>
+              </li>
+            </ul>
+            <form className="d-flex align-items-center ml-auto">
+              <div className="d-flex" style={{ position: "relative", left: 30 }}>
+                <ion-icon 
+                  style={{ fontSize: "1.4rem", color:"white" }} 
+                  name="search">
+                </ion-icon>
+              </div>
+              <input
+                className="form-inline"
+                type="text"
+                placeholder="Search..."
+              />
+            </form>
+          </div>
         </div>
       </header>
     );
