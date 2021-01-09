78fc8067fb6614806c93ab1c3fea049909cab15b
Ryan <ryan1230tenerife@gmail.com>
2021-01-09 21:24:55 +0000

    formatted to spaces

--- api/tag/model.php
+++ api/tag/model.php
@@ -2,53 +2,53 @@
 include_once "../config/Database.php";
 class TagModel extends Database {
 
-	protected function get_tags() {
-		$query = "SELECT * FROM tags";
-		$stmt = parent::connect()->query($query);
-		return $stmt;
-	}
+  protected function get_tags() {
+    $query = "SELECT * FROM tags";
+    $stmt = parent::connect()->query($query);
+    return $stmt;
+  }
 
-	/**
-	 * Get all tags from a single ticket
-	 */
-	protected function get_single_ticket(string $ticket_id) {
-		$query = "SELECT * FROM tags WHERE ticket_id = ? ORDER BY id DESC";
-		$conn = parent::connect();
-		$stmt = $conn->prepare($query);
-		$stmt->execute([$ticket_id]);
-		return $stmt;
-	}
+  /**
+   * Get all tags from a single ticket
+   */
+  protected function get_single_ticket(string $ticket_id) {
+    $query = "SELECT * FROM tags WHERE ticket_id = ? ORDER BY id DESC";
+    $conn = parent::connect();
+    $stmt = $conn->prepare($query);
+    $stmt->execute([$ticket_id]);
+    return $stmt;
+  }
 
-	/**
-	 * Add a tag to a ticket
-	 */
+  /**
+   * Add a tag to a ticket
+   */
   protected function post_tag(
-		string $ticket_id,
-		string $tag,
-		string $color
-	) {
-		$query = "INSERT INTO tags
-		SET
-			tag = :tag,
-			color = :color,
-			ticket_id = :ticket_id";
-		$conn = parent::connect();
-		$stmt = $conn->prepare($query);
-		$stmt->bindValue(":tag", $tag);
-		$stmt->bindValue(":ticket_id", $ticket_id);
-		$stmt->bindValue(":color", $color);
-		$stmt->execute();
-		return $stmt;
-	}
+    string $ticket_id,
+    string $tag,
+    string $color
+  ) {
+    $query = "INSERT INTO tags
+    SET
+      tag = :tag,
+      color = :color,
+      ticket_id = :ticket_id";
+    $conn = parent::connect();
+    $stmt = $conn->prepare($query);
+    $stmt->bindValue(":tag", $tag);
+    $stmt->bindValue(":ticket_id", $ticket_id);
+    $stmt->bindValue(":color", $color);
+    $stmt->execute();
+    return $stmt;
+  }
 
-	/**
-	 * Delete a tag
-	 */
-	protected function delete_tag(string $ticket_id, int $id) {
-		$query = "DELETE FROM tags WHERE ticket_id = ? AND id = ?";
-		$conn = parent::connect();
-		$stmt = $conn->prepare($query);
-		$stmt->execute([$ticket_id, $id]);
-		return $stmt;
-	}
+  /**
+   * Delete a tag
+   */
+  protected function delete_tag(string $ticket_id, int $id) {
+    $query = "DELETE FROM tags WHERE ticket_id = ? AND id = ?";
+    $conn = parent::connect();
+    $stmt = $conn->prepare($query);
+    $stmt->execute([$ticket_id, $id]);
+    return $stmt;
+  }
 }
--- api/tag/view.php
+++ api/tag/view.php
@@ -2,110 +2,110 @@
 include_once "./model.php";
 class TagView extends TagModel {
 
-	/**
-	 * Get all the tags
-	 */
-	public function show_tags() {
-		$results = parent::get_tags();
-		$num_of_rows = $results->rowCount();
+  /**
+   * Get all the tags
+   */
+  public function show_tags() {
+    $results = parent::get_tags();
+    $num_of_rows = $results->rowCount();
 
-		/**
-		 * Check if there are any tags
-		 */
-		if ($num_of_rows <= 0) {
-			echo json_encode(array(
-				"message" => "None found",
-				"data" => array()
-			));
-			exit;
-		}
+    /**
+     * Check if there are any tags
+     */
+    if ($num_of_rows <= 0) {
+      echo json_encode(array(
+        "message" => "None found",
+        "data" => array()
+      ));
+      exit;
+    }
 
-		/**
-		* Create array of tag-items
-		*/
-		$tags_array = array();
-		while ($row = $results->fetch(PDO::FETCH_ASSOC)) {
-			extract($row);
-			$tag_item = array(
-				"id" => $id,
-				"tag" => $tag,
-				"ticketId" => $ticket_id,
-				"color" => $color
-			);
-			array_push($tags_array, $tag_item);
-		}
+    /**
+    * Create array of tag-items
+    */
+    $tags_array = array();
+    while ($row = $results->fetch(PDO::FETCH_ASSOC)) {
+      extract($row);
+      $tag_item = array(
+        "id" => $id,
+        "tag" => $tag,
+        "ticketId" => $ticket_id,
+        "color" => $color
+      );
+      array_push($tags_array, $tag_item);
+    }
 
-		/**
-		 * Display successfull message
-		 */
-		echo json_encode(array(
-			"count" => $num_of_rows,
-			"success" => true,
-			"data" => $tags_array
-		));
-		exit;
-	}
+    /**
+     * Display successfull message
+     */
+    echo json_encode(array(
+      "count" => $num_of_rows,
+      "success" => true,
+      "data" => $tags_array
+    ));
+    exit;
+  }
 
-	/**
-	 * Get all the tags that belong to a single ticket
-	 */
-	public function show_single_tag(string $ticket_id) {
-		$results = parent::get_single_ticket($ticket_id);
-		$num_of_rows = $results->rowCount();
+  /**
+   * Get all the tags that belong to a single ticket
+   */
+  public function show_single_tag(string $ticket_id) {
+    $results = parent::get_single_ticket($ticket_id);
+    $num_of_rows = $results->rowCount();
 
-		/**
-		 * Check if there are any tags
-		 */
-		if ($num_of_rows <= 0) {
-			echo json_encode(array(
-				"message" => "None found",
-				"data" => array()
-			));
-			exit;
-		}
+    /**
+     * Check if there are any tags
+     */
+    if ($num_of_rows <= 0) {
+      echo json_encode(array(
+        "message" => "None found",
+        "data" => array()
+      ));
+      exit;
+    }
 
-		/**
-		 * Create array of tag-items
-		 */
-		$tags_array = array();
-		while ($row = $results->fetch(PDO::FETCH_ASSOC)) {
-			extract($row);
-			$tag_item = array(
-				"id" => $id,
-				"tag" => $tag,
-				"ticketId" => $ticket_id,
-				"color" => $color
-			);
-			array_push($tags_array, $tag_item);
-		}
+    /**
+     * Create array of tag-items
+     */
+    $tags_array = array();
+    while ($row = $results->fetch(PDO::FETCH_ASSOC)) {
+      extract($row);
+      $tag_item = array(
+        "id" => $id,
+        "tag" => $tag,
+        "ticketId" => $ticket_id,
+        "color" => $color
+      );
+      array_push($tags_array, $tag_item);
+    }
 
-		/**
-		 * Display successfull message
-		 */
-		echo json_encode(array(
-			"count" => $num_of_rows,
-			"success" => true,
-			"data" => $tags_array
-		));
-		exit;
-	}
+    /**
+     * Display successfull message
+     */
+    echo json_encode(array(
+      "count" => $num_of_rows,
+      "success" => true,
+      "data" => $tags_array
+    ));
+    exit;
+  }
 
-	/**
-	 * Create a tag
-	 */
-	public function add_tag(string $ticket_id, string $tag, string $color) {
-		$results = parent::post_tag($ticket_id, $tag, $color);
-		if (!$results) return false;
-		else return true;
-	}
+  /**
+   * Create a tag
+   */
+  public function add_tag(string $ticket_id, string $tag, string $color) {
+    $results = parent::post_tag($ticket_id, $tag, $color);
+    if (!$results) return false;
+    else return true;
+  }
 
-	/**
-	 * Delete a tag
-	 */
-	public function remove_tag(string $ticket_id, int $id) {
-		$results = parent::delete_tag($ticket_id, $id);
-		if (!$results) return false;
-		else return true;
-	}
+  /**
+   * Delete a tag
+   */
+  public function remove_tag(string $ticket_id, int $id) {
+    $results = parent::delete_tag($ticket_id, $id);
+    if (!$results) return false;
+    else return true;
+  }
 
 }
